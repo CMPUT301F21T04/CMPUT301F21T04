a6397c7952870a2fb1c2dc3bc3af72214ad8677c
Rachel <45080402+rachelraso@users.noreply.github.com>
2021-10-04 01:06:24 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301f21t04/Shape.java
+++ app/src/main/java/com/example/cmput301f21t04/Shape.java
@@ -3,11 +3,7 @@ package com.example.cmput301f21t04;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
 
     private String color = "Green";
-=======
-    private String color = "red";
->>>>>>> parent of e739a72 (Update Shape.java)
 
 }
